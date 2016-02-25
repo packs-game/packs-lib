c6da68a4575519e14e10f4d355134bada95d46ee
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-02-24 22:52:30 -0500

    fix sending error

--- src/check-super-admin.js
+++ src/check-super-admin.js
@@ -2,7 +2,7 @@ var api = require('./api');
 
 //express middleware to check auth credentials
 function auth(req, res, next) {
-	if (!req.body || !req.body.id || !req.body.token) { return res.send(400).send('Missing id or token in request.'); }
+	if (!req.body || !req.body.id || !req.body.token) { return res.status(400).send('Missing id or token in request.'); }
 	api.checkAuth(req.body.token, function(err,user){
 		if (!user.superAdmin) { return res.status(400).send('User not superadmin.'); }
 		if (err) { return res.sendStatus(400); }
