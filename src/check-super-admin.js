959120e066c1f2bfb29a39b654fc2c571899b4b4
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-02-24 22:20:17 -0500

    add better msg to fails

--- src/check-super-admin.js
+++ src/check-super-admin.js
@@ -2,9 +2,9 @@ var api = require('./api');
 
 //express middleware to check auth credentials
 function auth(req, res, next) {
-	if (!req.body || !req.body.id || !req.body.token) { return res.sendStatus(400); }
+	if (!req.body || !req.body.id || !req.body.token) { return res.send(400).send('Missing id or token in request.'); }
 	api.checkAuth(req.body.token, function(err,user){
-		if (!user.superAdmin) { return res.sendStatus(400); }
+		if (!user.superAdmin) { return res.status(400).send('User not superadmin.'); }
 		if (err) { return res.sendStatus(400); }
 		next();
 	});
